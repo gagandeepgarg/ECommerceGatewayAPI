ea68bd653c09cbe4666289a2a7c071459d75fbc9
ggarg-c <ggarg-c@alierahealthcare.com>
2019-05-06 10:24:51 +0530

    working nconfiguratio  updated for items and orders microservice

--- ECommerceGatewayAPI/Startup.cs
+++ ECommerceGatewayAPI/Startup.cs
@@ -27,6 +27,7 @@ namespace ECommerceGatewayAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
             services.AddOcelot(Configuration);
         }
 
@@ -42,10 +43,15 @@ namespace ECommerceGatewayAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-            await app.UseOcelot();
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowAnyOrigin();
+            });
+            // app.UseHttpsRedirection();
+                app.UseMvc();
+                await app.UseOcelot();
         }
     }
 }
